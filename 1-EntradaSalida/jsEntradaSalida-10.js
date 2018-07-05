768ca4604b043d8e770074cf09b65a39c8e2d6d4
413831 <40808636+413831@users.noreply.github.com>
2018-07-04 22:11:13 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -3,6 +3,14 @@ Transformarlo a entero (parseInt), luego
 mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
-{
+{var precio = document.getElementById("importe").value;
+var descuento;
+ 
+precio = parseInt(precio);
+
+descuento = precio * 0.25;
+descuento = precio - descuento;
+ 
+ document.getElementById("resultado").value = descuento;
 	
 }
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -3,7 +3,13 @@ transformarlo a entero (parseInt), luego
 mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
-{
+{var sueldo = document.getElementById("sueldo").value;
+
+sueldo = parseInt(sueldo);
+sueldo = sueldo * 1.1;
+
+alert(sueldo);
+
 
 	
 }
