ceb0138b2c29b7656c8b832d2a57fc8b3ed916ba
413831 <40808636+413831@users.noreply.github.com>
2018-07-17 16:01:28 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -2,7 +2,16 @@
 transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
-{
+{var num1 = document.getElementById("numeroUno").value;
+var num2 = document.getElementById("numeroDos").value;
+var resultado;
+
+num1 = parseInt(num1);
+num2 = parseInt(num2);
+
+resultado = num1+num2;
+
+alert("la suma es "+resultado);
 
 }
 
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
@@ -1,4 +1,27 @@
 function mostrar()
-{
+{var planeta;
 
+planeta = prompt ("Ingrese un planeta del sistema solar");
+
+switch (planeta)
+{
+	case "venus" :
+	case "mercurio" :
+	case "marte" :
+	alert("Acá hace más calor")
+	break;
+	case "tierra" :
+	alert("Acá vivimos nosotros")
+	break;
+	case "urano" :
+	case "jupiter" :
+	case "saturno" :
+	case "neptuno" :
+	alert("Acá hace más frío")
+	break;
+	default :
+	alert("Ese no es un planeta")
+	break;
+
+}
 }
